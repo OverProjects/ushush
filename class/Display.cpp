ba7cae2210aae4e29a120ebf224cb25900ef3f8d
Overeath <bastdav@hotmail.fr>
2019-04-15 13:35:27 +0200

    Les commandes, l'obj

--- class/Display.cpp
+++ class/Display.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 
 #include "Display.hpp"
 
 bool Display::init(std::string path)
 {
-    if (!m_texture.loadFromFile(path)) {std::cerr << "no load for " << path << std::endl;}
+    if (!m_texture.loadFromFile(path)) {std::cerr << "no load for " << path << std::endl;
+                                        return false;}
     m_sprite.setTexture(m_texture);
 
     m_size.x = m_sprite.getGlobalBounds().width;
     m_size.y = m_sprite.getGlobalBounds().height;
+
+    return true;
 }
 
 void Display::show(sf::RenderWindow &window)
